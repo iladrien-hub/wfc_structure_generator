2d6eaf63dae2bf830ce2764c0699115b0cf9f9a9
iladrien-hub <sliptchenko71@gmail.com>
2020-11-21 14:31:59 +0200

    Додані методи для повороту на 180 і 270 градусів

--- src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/RotateStructure.java
+++ src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/RotateStructure.java
@@ -3,7 +3,7 @@ package ua.iladrien.wfcstructuregenerator.structuregen;
 import net.minecraft.block.BlockState;
 
 public class RotateStructure {
-    public static BlockState[][][] rotate(BlockState[][][] initial) {
+    public static BlockState[][][] rotate90(BlockState[][][] initial) {
         int size_y = initial.length;
         int size_x = initial[0].length;
         int size_z = initial[0][0].length;
@@ -12,6 +12,36 @@ public class RotateStructure {
             for (int x = 0; x < size_x; x++) {
                 for (int z = 0; z < size_z; z++) {
                     result[y][z][x] = initial[y][x][z];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static BlockState[][][] rotate180(BlockState[][][] initial) {
+        int size_y = initial.length;
+        int size_x = initial[0].length;
+        int size_z = initial[0][0].length;
+        BlockState[][][] result = new BlockState[size_y][size_x][size_z];
+        for (int y = 0; y < size_y; y++) {
+            for (int x = 0; x < size_x; x++) {
+                for (int z = 0; z < size_z; z++) {
+                    result[y][size_x-x-1][size_z-z-1] = initial[y][x][z];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static BlockState[][][] rotate270(BlockState[][][] initial) {
+        int size_y = initial.length;
+        int size_x = initial[0].length;
+        int size_z = initial[0][0].length;
+        BlockState[][][] result = new BlockState[size_y][size_z][size_x];
+        for (int y = 0; y < size_y; y++) {
+            for (int x = 0; x < size_x; x++) {
+                for (int z = 0; z < size_z; z++) {
+                    result[y][size_z-z-1][size_x-x-1] = initial[y][x][z];
                 }
             }
         }
