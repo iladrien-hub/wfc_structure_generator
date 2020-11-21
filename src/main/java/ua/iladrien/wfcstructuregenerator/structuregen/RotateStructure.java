c5d226d7f0362990682090af0f0568731cc5a8fa
iladrien-hub <sliptchenko71@gmail.com>
2020-11-21 21:02:38 +0200

    Додано поворот для блоків з HORIZONTAL_FACING

--- src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/RotateStructure.java
+++ src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/RotateStructure.java
@@ -1,8 +1,35 @@
 package ua.iladrien.wfcstructuregenerator.structuregen;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.util.Direction;
 
 public class RotateStructure {
+
+    private static final Direction[] directions = new Direction[] {
+            Direction.WEST,
+            Direction.SOUTH,
+            Direction.EAST,
+            Direction.NORTH
+    };
+
+    private static int fromVal(Direction dir) {
+        for (int i = 0; i < directions.length; i++) {
+            if (directions[i] == dir) return i;
+        }
+        return -1;
+    }
+
+    public static BlockState update(BlockState initial, int angle) {
+        // HORIZONTAL_FACING
+        try {
+            Direction facing = initial.get(HorizontalBlock.HORIZONTAL_FACING);
+            int facingIn = (fromVal(facing) + angle) % 4;
+            initial = initial.with(HorizontalBlock.HORIZONTAL_FACING, directions[facingIn]);
+        } catch (Exception ignored) { }
+        return initial;
+    }
+
     public static BlockState[][][] rotate90(BlockState[][][] initial) {
         int size_y = initial.length;
         int size_x = initial[0].length;
@@ -11,7 +38,7 @@ public class RotateStructure {
         for (int y = 0; y < size_y; y++) {
             for (int x = 0; x < size_x; x++) {
                 for (int z = 0; z < size_z; z++) {
-                    result[y][z][size_x-x-1] = initial[y][x][z];
+                    result[y][z][size_x-x-1] = update(initial[y][x][z], 1);
                 }
             }
         }
@@ -26,7 +53,7 @@ public class RotateStructure {
         for (int y = 0; y < size_y; y++) {
             for (int x = 0; x < size_x; x++) {
                 for (int z = 0; z < size_z; z++) {
-                    result[y][size_x-x-1][size_z-z-1] = initial[y][x][z];
+                    result[y][size_x-x-1][size_z-z-1] = update(initial[y][x][z], 2);
                 }
             }
         }
@@ -41,7 +68,7 @@ public class RotateStructure {
         for (int y = 0; y < size_y; y++) {
             for (int x = 0; x < size_x; x++) {
                 for (int z = 0; z < size_z; z++) {
-                    result[y][z][x] = initial[y][x][z];
+                    result[y][size_z-z-1][x] = update(initial[y][x][z], 3);
                 }
             }
         }
