4b8972f7c7b3107d166fb2b90df99332ef662dae
iladrien-hub <sliptchenko71@gmail.com>
2020-11-21 14:31:21 +0200

    Переписаний метод getMax і перейменований в getUnfinished

--- src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/Generator.java
+++ src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/Generator.java
@@ -24,15 +24,39 @@ public class Generator {
                 for (int z = 0; z < size_Z; z++) {
                     result[y][x][z] = new ListHolder();
                     result[y][x][z].list = Tiles.getRegistered();
+                    if (y == 0) {
+                        result[y][x][z].list.removeIf(tile -> !tile.isAllowedOnGround());
+                    } else {
+                        result[y][x][z].list.removeIf(Tile::isOnGroundOnly);
+                    }
                 }
-        for (int x = 0; x < size_X; x++)
-            for (int z = 0; z < size_Z; z++) {
-                result[0][x][z].list.removeIf(tile -> !tile.isAllowedOnGround());
-            }
         return result;
     }
 
     private static final Random RANDOM = new Random(System.currentTimeMillis());
+
+    private static class dataItemStruct {
+        int x, y, z;
+        ArrayList<Tile> list;
+    }
+
+    private dataItemStruct getUnfinished() {
+        dataItemStruct max = new dataItemStruct();
+        max.x = 0;
+        max.y = 0;
+        max.z = 0;
+        max.list = data[0][0][0].list;
+        for (int y = 0; y < size_Y; y++)
+            for (int x = 0; x < size_X; x++)
+                for (int z = 0; z < size_Z; z++)
+                    if (data[y][x][z].list.size() > 1 && checkCollisions(x, y, z)) {
+                        max.x = x;
+                        max.y = y;
+                        max.z = z;
+                        max.list = data[y][x][z].list;
+                    }
+        return max;
+    }
 
     public void generate() {
         int y = 0;
@@ -41,6 +65,13 @@ public class Generator {
         data = initializeData();
         data[y][x][z].list.removeIf(tile -> tile != Tiles.FANCY_STONE_BASE);
         collapse(x, y, z);
+        dataItemStruct max = getUnfinished();
+        while (max.list.size() > 1) {
+            Tile random = max.list.get(RANDOM.nextInt(max.list.size()));
+            max.list.removeIf(tile -> tile != random);
+            collapse(max.x, max.y, max.z);
+            max = getUnfinished();
+        }
     }
 
     private void doCollapse(int x, int y, int z, ArrayList<Tile> comparable) {
@@ -62,13 +93,13 @@ public class Generator {
     }
 
     public void place(World world, BlockPos pos) {
-        int center_x = size_X * Tile.WIDTH / 2;
-        int center_z = size_Z * Tile.WIDTH / 2;
+        int center_x = -1 + size_X * Tile.WIDTH / 2;
+        int center_z = -1 + size_Z * Tile.WIDTH / 2;
         for (int y = 0; y < size_Y; y++) {
             for (int x = 0; x < size_X; x++) {
                 for (int z = 0; z < size_Z; z++) {
                     if (data[y][x][z].list.size() == 1)
-                        data[y][x][z].list.get(0).placeAt(world, pos.add(Tile.WIDTH*x-center_x, Tile.HEIGHT*y, Tile.WIDTH*z-center_z));
+                        data[y][x][z].list.get(0).placeAt(world, pos.add(Tile.WIDTH*x-center_x-x, Tile.HEIGHT*y, Tile.WIDTH*z-center_z-z));
                 }
             }
         }
@@ -122,4 +153,17 @@ public class Generator {
         return x >= 0 && y >=0 && z >=0 && x < size_X && y < size_Y && z < size_Z;
     }
 
+    private boolean checkCollisions(int x, int y, int z) {
+        if (checkCoordinates(x, y + 1, z) && data[x][y + 1][z].list.size() == 1)
+            return true;
+        if (checkCoordinates(x, y - 1, z) && data[x][y - 1][z].list.size() == 1)
+            return true;
+        if (checkCoordinates(x + 1, y, z) && data[x + 1][y][z].list.size() == 1)
+            return true;
+        if (checkCoordinates(x - 1, y, z) && data[x - 1][y][z].list.size() == 1)
+            return true;
+        if (checkCoordinates(x, y, z + 1) && data[x][y][z + 1].list.size() == 1)
+            return true;
+        return checkCoordinates(x, y, z - 1) && data[x][y][z - 1].list.size() == 1;
+    }
 }
