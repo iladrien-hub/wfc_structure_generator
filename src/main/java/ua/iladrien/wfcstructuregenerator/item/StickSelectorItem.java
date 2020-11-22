0420385364d802ec54e456a29a695f84bcc315f8
iladrien-hub <sliptchenko71@gmail.com>
2020-11-22 15:37:04 +0200

    Додано CHEST_TYPE, SOUTH, EAST, WEST, PERSISTENT, DOUBLE_BLOCK_HALF

--- src/main/java/ua/iladrien/wfcstructuregenerator/item/StickSelectorItem.java
+++ src/main/java/ua/iladrien/wfcstructuregenerator/item/StickSelectorItem.java
@@ -2,11 +2,14 @@ package ua.iladrien.wfcstructuregenerator.item;
 
 import com.google.common.collect.ImmutableMap;
 import mcp.MethodsReturnNonnullByDefault;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.state.Property;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.ChestType;
+import net.minecraft.state.properties.DoubleBlockHalf;
+import net.minecraft.state.properties.Half;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -76,31 +79,57 @@ public class StickSelectorItem extends Item {
         fw.write("data["+y+"]["+x+"]["+z+"] = ");
         fw.write(variable);
         for (Map.Entry<Property<?>, Comparable<?>> item: values.entrySet()) {
-            switch (item.getKey().getName()) {
-                case "facing":
-                    fw.write(".with(HorizontalBlock.HORIZONTAL_FACING, net.minecraft.util.Direction." + item.getValue().toString().toUpperCase() + ")");
-                    break;
-                case "half":
-                    fw.write(".with(BlockStateProperties.HALF, Half." + item.getValue().toString().toUpperCase() + ")");
-                    break;
-                case "axis":
-                    fw.write(".with(RotatedPillarBlock.AXIS, net.minecraft.util.Direction.Axis." + item.getValue().toString().toUpperCase() + ")");
-                    break;
-                case "open":
-                    fw.write(".with(TrapDoorBlock.OPEN, " + item.getValue() + ")");
-                    break;
-                case "type":
-                    fw.write(".with(SlabBlock.TYPE, SlabType." + item.getValue().toString().toUpperCase() + ")");
-                    break;
-                case "hinge":
-                    fw.write(".with(DoorBlock.HINGE, DoorHingeSide." + item.getValue().toString().toUpperCase() + ")");
-                    break;
-                case "shape":
-                    fw.write(".with(StairsBlock.SHAPE, StairsShape." + item.getValue().toString().toUpperCase() + ")");
-                    break;
-                default:
-                    WFCStructureGenerator.log(Level.WARN, "Uncaught property \""+ item.getKey().getName() +"\" = " + item.getValue().toString());
+            Class<?> clazz = item.getKey().getValueClass();
+            String name = item.getKey().getName();
+            if (clazz == BlockStateProperties.HORIZONTAL_FACING.getValueClass()) {
+                fw.write(".with(BlockStateProperties.HORIZONTAL_FACING, net.minecraft.util.Direction." + item.getValue().toString().toUpperCase() + ")");
             }
+            else if (clazz == BlockStateProperties.DOUBLE_BLOCK_HALF.getValueClass()) {
+                fw.write(".with(BlockStateProperties.DOUBLE_BLOCK_HALF, DoubleBlockHalf." + item.getValue().toString().toUpperCase() + ")");
+            }
+            else if (clazz == BlockStateProperties.HALF.getValueClass()) {
+                fw.write(".with(BlockStateProperties.HALF, Half." + item.getValue().toString().toUpperCase() + ")");
+            }
+            else if (clazz == BlockStateProperties.AXIS.getValueClass()) {
+                fw.write(".with(BlockStateProperties.AXIS, net.minecraft.util.Direction.Axis." + item.getValue().toString().toUpperCase() + ")");
+            }
+            else if (name.equals("open")) {
+                fw.write(".with(BlockStateProperties.OPEN, " + item.getValue() + ")");
+            }
+            else if (clazz == BlockStateProperties.SLAB_TYPE.getValueClass()) {
+                fw.write(".with(BlockStateProperties.SLAB_TYPE, SlabType." + item.getValue().toString().toUpperCase() + ")");
+            }
+            else if (clazz == BlockStateProperties.DOOR_HINGE.getValueClass()) {
+                fw.write(".with(BlockStateProperties.DOOR_HINGE, DoorHingeSide." + item.getValue().toString().toUpperCase() + ")");
+            }
+            else if (clazz == BlockStateProperties.STAIRS_SHAPE.getValueClass()) {
+                fw.write(".with(BlockStateProperties.STAIRS_SHAPE, StairsShape." + item.getValue().toString().toUpperCase() + ")");
+            }
+            else if (clazz == BlockStateProperties.CHEST_TYPE.getValueClass()) {
+                fw.write(".with(BlockStateProperties.CHEST_TYPE, ChestType." + item.getValue().toString().toUpperCase() + ")");
+            }
+            else if (name.equals("north")) {
+                fw.write(".with(BlockStateProperties.NORTH, " + item.getValue().toString() + ")");
+            }
+            else if (name.equals("south")) {
+                fw.write(".with(BlockStateProperties.SOUTH, " + item.getValue().toString() + ")");
+            }
+            else if (name.equals("east")) {
+                fw.write(".with(BlockStateProperties.EAST, " + item.getValue().toString() + ")");
+            }
+            else if (name.equals("west")) {
+                fw.write(".with(BlockStateProperties.WEST, " + item.getValue().toString() + ")");
+            }
+            else if (name.equals("persistent")) {
+                fw.write(".with(BlockStateProperties.PERSISTENT, " + item.getValue().toString() + ")");
+            }
+            else {
+                WFCStructureGenerator.log(Level.WARN, "Uncaught property \""+ item.getKey().getName() +"\" = " + item.getValue().toString());
+            }
+            try {
+                BlockState bs = Blocks.OAK_LEAVES.getDefaultState();
+//                bs = bs.with(BlockStateProperties.CHEST_TYPE, ChestType.SINGLE);
+            } catch (Exception exception) { exception.printStackTrace(); }
         }
 
         fw.append(";\n");
