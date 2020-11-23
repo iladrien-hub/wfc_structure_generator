fad0123768efcfb75a4da1c2e7d412a26c4344b8
iladrien-hub <sliptchenko71@gmail.com>
2020-11-23 16:21:51 +0200

    TileRotation::fromAngle(int angle)

--- src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/tile/TileRotation.java
+++ src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/tile/TileRotation.java
@@ -5,6 +5,16 @@ public enum TileRotation {
 
     public final int angle;
 
+    public static TileRotation fromAngle(int angle) {
+        switch (angle) {
+            case 0: return ROTATION0;
+            case 1: return ROTATION90;
+            case 2: return ROTATION180;
+            case 3: return ROTATION270;
+        }
+        return null;
+    }
+
     TileRotation(int angle) {
         this.angle = angle;
     }
