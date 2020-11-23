1c8ea843037103b231fb3a74e9f0904670f2b6ca
iladrien-hub <sliptchenko71@gmail.com>
2020-11-23 16:19:13 +0200

    Miscellaneous

---
+++ src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/tile/tiles/misc/Miscellaneous.java
@@ -0,0 +1,8 @@
+package ua.iladrien.wfcstructuregenerator.structuregen.tile.tiles.misc;
+
+import ua.iladrien.wfcstructuregenerator.structuregen.tile.Tile;
+
+public class Miscellaneous {
+    public static final Tile Misc_0007 = new Misc_0007();
+    public static final Tile Misc_0008 = new Misc_0008();
+}
