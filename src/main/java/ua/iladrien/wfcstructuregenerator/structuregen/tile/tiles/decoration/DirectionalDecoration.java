9ed7ef274a2ee0e9ecc9a6e6f9c7a9f76407841f
iladrien-hub <sliptchenko71@gmail.com>
2020-11-23 16:16:51 +0200

    DirectionalDecoration::checkOutStrictConditions()

--- src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/tile/tiles/decoration/DirectionalDecoration.java
+++ src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/tile/tiles/decoration/DirectionalDecoration.java
@@ -1,8 +1,18 @@
 package ua.iladrien.wfcstructuregenerator.structuregen.tile.tiles.decoration;
 
+import net.minecraft.util.math.vector.Vector3i;
+import ua.iladrien.wfcstructuregenerator.structuregen.Generator;
+import ua.iladrien.wfcstructuregenerator.structuregen.LazyStuff;
 import ua.iladrien.wfcstructuregenerator.structuregen.tile.tiles.Tiles;
 
 public abstract class DirectionalDecoration extends Decoration {
+
+    @Override
+    public boolean checkOutStrictConditions(Vector3i pos, Generator generator) {
+        Vector3i left = directions[(rotation.angle + 1) % 4];
+        return generator.checkCoordinates(LazyStuff.addVectors3(pos, left));
+    }
+
     @Override
     public void registerVariants() {
         super.registerVariants();
