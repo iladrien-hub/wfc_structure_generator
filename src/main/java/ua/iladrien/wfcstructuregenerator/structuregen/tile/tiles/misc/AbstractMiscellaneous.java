f8925b68205165ea3a246174e91834a1a25497d0
iladrien-hub <sliptchenko71@gmail.com>
2020-11-24 15:42:03 +0200

    AbstractMiscellaneous

---
+++ src/main/java/ua/iladrien/wfcstructuregenerator/structuregen/tile/tiles/misc/AbstractMiscellaneous.java
@@ -0,0 +1,9 @@
+package ua.iladrien.wfcstructuregenerator.structuregen.tile.tiles.misc;
+
+import ua.iladrien.wfcstructuregenerator.structuregen.tile.Tile;
+
+public abstract class AbstractMiscellaneous extends Tile {
+    @Override
+    public void registerVariants() {
+    }
+}
