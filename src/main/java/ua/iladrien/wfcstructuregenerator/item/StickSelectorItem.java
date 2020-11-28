5e06b8e8ac87b6166dc7b6c0da37278a69b817f4
iladrien-hub <sliptchenko71@gmail.com>
2020-11-28 20:56:23 +0200

    Selector code clean up

--- src/main/java/ua/iladrien/wfcstructuregenerator/item/StickSelectorItem.java
+++ src/main/java/ua/iladrien/wfcstructuregenerator/item/StickSelectorItem.java
@@ -51,10 +51,6 @@ public class StickSelectorItem extends Item {
     }
 
     public void save(BlockPos pos, World world) {
-//        File output = new File("/");
-//
-//        WFCStructureGenerator.log(Level.INFO, output.getAbsolutePath());
-
         try(FileWriter fw = new FileWriter("output.java", false)) {
             fw.write("BlockState[][][] data = new BlockState[HEIGHT][WIDTH][WIDTH];\n");
             for (int y = 0; y < height; y++) {
@@ -79,65 +75,65 @@ public class StickSelectorItem extends Item {
         fw.write("data["+y+"]["+x+"]["+z+"] = ");
         fw.write(variable);
         for (Map.Entry<Property<?>, Comparable<?>> item: values.entrySet()) {
-            Class<?> clazz = item.getKey().getValueClass();
-            String name = item.getKey().getName();
-            if (clazz == BlockStateProperties.HORIZONTAL_FACING.getValueClass()) {
+            Property<?> key = item.getKey();
+            if (key == BlockStateProperties.HORIZONTAL_FACING) {
                 fw.write(".with(BlockStateProperties.HORIZONTAL_FACING, net.minecraft.util.Direction." + item.getValue().toString().toUpperCase() + ")");
-            }
-            else if (clazz == BlockStateProperties.DOUBLE_BLOCK_HALF.getValueClass()) {
+            } else if (key == BlockStateProperties.FACING) {
+                fw.write(".with(BlockStateProperties.FACING, net.minecraft.util.Direction." + item.getValue().toString().toUpperCase() + ")");
+            } else if (key == BlockStateProperties.DOUBLE_BLOCK_HALF) {
                 fw.write(".with(BlockStateProperties.DOUBLE_BLOCK_HALF, DoubleBlockHalf." + item.getValue().toString().toUpperCase() + ")");
-            }
-            else if (clazz == BlockStateProperties.HALF.getValueClass()) {
+            } else if (key == BlockStateProperties.HALF) {
                 fw.write(".with(BlockStateProperties.HALF, Half." + item.getValue().toString().toUpperCase() + ")");
-            }
-            else if (clazz == BlockStateProperties.AXIS.getValueClass()) {
+            } else if (key == BlockStateProperties.AXIS) {
                 fw.write(".with(BlockStateProperties.AXIS, net.minecraft.util.Direction.Axis." + item.getValue().toString().toUpperCase() + ")");
-            }
-            else if (name.equals("open")) {
+            } else if (key == BlockStateProperties.OPEN) {
                 fw.write(".with(BlockStateProperties.OPEN, " + item.getValue() + ")");
-            }
-            else if (clazz == BlockStateProperties.SLAB_TYPE.getValueClass()) {
+            } else if (key == BlockStateProperties.SLAB_TYPE) {
                 fw.write(".with(BlockStateProperties.SLAB_TYPE, SlabType." + item.getValue().toString().toUpperCase() + ")");
-            }
-            else if (clazz == BlockStateProperties.DOOR_HINGE.getValueClass()) {
+            } else if (key == BlockStateProperties.DOOR_HINGE) {
                 fw.write(".with(BlockStateProperties.DOOR_HINGE, DoorHingeSide." + item.getValue().toString().toUpperCase() + ")");
-            }
-            else if (clazz == BlockStateProperties.STAIRS_SHAPE.getValueClass()) {
+            } else if (key == BlockStateProperties.STAIRS_SHAPE) {
                 fw.write(".with(BlockStateProperties.STAIRS_SHAPE, StairsShape." + item.getValue().toString().toUpperCase() + ")");
-            }
-            else if (clazz == BlockStateProperties.CHEST_TYPE.getValueClass()) {
+            } else if (key == BlockStateProperties.CHEST_TYPE) {
                 fw.write(".with(BlockStateProperties.CHEST_TYPE, ChestType." + item.getValue().toString().toUpperCase() + ")");
-            }
-            else if (clazz == BlockStateProperties.FACE.getValueClass()) {
+            } else if (key == BlockStateProperties.FACE) {
                 fw.write(".with(BlockStateProperties.FACE, AttachFace." + item.getValue().toString().toUpperCase() + ")");
-            }
-            else if (name.equals("north")) {
+            } else if (key == BlockStateProperties.BELL_ATTACHMENT) {
+                fw.write(".with(BlockStateProperties.BELL_ATTACHMENT, BellAttachment." + item.getValue().toString().toUpperCase() + ")");
+            } else if (key == BlockStateProperties.field_235909_T_) {
+                fw.write(".with(BlockStateProperties.field_235909_T_,  WallHeight." + item.getValue().toString().toUpperCase() + ")");
+            } else if (key == BlockStateProperties.NORTH) {
                 fw.write(".with(BlockStateProperties.NORTH, " + item.getValue().toString() + ")");
-            }
-            else if (name.equals("south")) {
+            } else if (key == BlockStateProperties.field_235910_U_) {
+                fw.write(".with(BlockStateProperties.field_235910_U_,  WallHeight." + item.getValue().toString().toUpperCase() + ")");
+            } else if (key == BlockStateProperties.SOUTH) {
                 fw.write(".with(BlockStateProperties.SOUTH, " + item.getValue().toString() + ")");
-            }
-            else if (name.equals("east")) {
+            } else if (key == BlockStateProperties.field_235908_S_) {
+                fw.write(".with(BlockStateProperties.field_235908_S_,  WallHeight." + item.getValue().toString().toUpperCase() + ")");
+            } else if (key == BlockStateProperties.EAST) {
                 fw.write(".with(BlockStateProperties.EAST, " + item.getValue().toString() + ")");
-            }
-            else if (name.equals("west")) {
-                fw.write(".with(BlockStateProperties.WEST, " + item.getValue().toString() + ")");
-            }
-            else if (name.equals("persistent")) {
+            } else if (key == BlockStateProperties.field_235911_V_) {
+                fw.write(".with(BlockStateProperties.field_235911_V_,  WallHeight." + item.getValue().toString().toUpperCase() + ")");
+            } else if (key == BlockStateProperties.WEST) {
+                    fw.write(".with(BlockStateProperties.WEST, " + item.getValue().toString() + ")");
+            } else if (key == BlockStateProperties.UP) {
+                fw.write(".with(BlockStateProperties.UP, " + item.getValue().toString() + ")");
+            } else if (key == BlockStateProperties.LIT) {
+                fw.write(".with(BlockStateProperties.LIT, " + item.getValue().toString() + ")");
+            } else if (key == BlockStateProperties.PERSISTENT) {
                 fw.write(".with(BlockStateProperties.PERSISTENT, " + item.getValue().toString() + ")");
-            }
-            else if (name.equals("hanging")) {
+            } else if (key == BlockStateProperties.PICKLES_1_4) {
+                fw.write(".with(BlockStateProperties.PICKLES_1_4, " + item.getValue().toString() + ")");
+            } else if (key == BlockStateProperties.HANGING) {
                 fw.write(".with(BlockStateProperties.HANGING, " + item.getValue().toString() + ")");
+            } else if (key == BlockStateProperties.AGE_0_3) {
+                fw.write(".with(BlockStateProperties.AGE_0_3, " + item.getValue().toString() + ")");
+            } else if (key == BlockStateProperties.WATERLOGGED) {
+                fw.write(".with(BlockStateProperties.WATERLOGGED, " + item.getValue().toString() + ")");
+            } else {
+                WFCStructureGenerator.log(Level.WARN, "Uncaught property \""+ key.getName() +"\" = " + item.getValue().toString());
             }
-            else {
-                WFCStructureGenerator.log(Level.WARN, "Uncaught property \""+ item.getKey().getName() +"\" = " + item.getValue().toString());
-            }
-            try {
-                BlockState bs = Blocks.OAK_LEAVES.getDefaultState();
-//                bs = bs.with(BlockStateProperties.CHEST_TYPE, ChestType.SINGLE);
-            } catch (Exception exception) { exception.printStackTrace(); }
         }
-
         fw.append(";\n");
     }
 }
